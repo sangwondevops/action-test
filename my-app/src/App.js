d20914cd8cc19a50dbe334f8bed41c432f948600
DevOpsSangwon <132826554+DevOpsSangwon@users.noreply.github.com>
2023-05-30 16:08:59 +0900

    Update App.js

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn Github Action 
+          Learn React
         </a>
       </header>
     </div>
