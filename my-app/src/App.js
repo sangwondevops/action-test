0ec2d5e01a924cfea79452f5551748d4fa8bef90
DevOpsSangwon <132826554+DevOpsSangwon@users.noreply.github.com>
2023-05-30 16:01:25 +0900

    Update App.js

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn Github Action 
+          Learn Github Action CICD
         </a>
       </header>
     </div>
