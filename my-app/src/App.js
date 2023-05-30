82303a3976efb79e68a0f4d2e7ad80d89be30b72
DevOpsSangwon <132826554+DevOpsSangwon@users.noreply.github.com>
2023-05-30 16:39:51 +0900

    Add basic_file

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn Action
+          Learn Action 2
         </a>
       </header>
     </div>
